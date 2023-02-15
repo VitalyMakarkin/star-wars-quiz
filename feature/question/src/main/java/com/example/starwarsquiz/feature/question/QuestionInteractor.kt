e432582cc8b9157eb9baff9fcbbe219873010e38
vitalymakarkin <mak.nk16rus@gmail.com>
2023-02-15 18:07:20 +0300

    Add new methods

--- feature/question/src/main/java/com/example/starwarsquiz/feature/question/QuestionInteractor.kt
+++ feature/question/src/main/java/com/example/starwarsquiz/feature/question/QuestionInteractor.kt
@@ -1,8 +1,14 @@
 package com.example.starwarsquiz.feature.question
 
 import com.example.starwarsquiz.core.data.repository.PeopleRepository
+import com.example.starwarsquiz.core.model.data.Person
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class QuestionInteractor @Inject constructor(
     private val peopleRepository: PeopleRepository
-)
+) {
+    suspend fun getPeople(): Flow<List<Person>> {
+        return peopleRepository.getPeople()
+    }
+}
--- feature/question/src/main/java/com/example/starwarsquiz/feature/question/QuestionViewModel.kt
+++ feature/question/src/main/java/com/example/starwarsquiz/feature/question/QuestionViewModel.kt
@@ -1,10 +1,35 @@
 package com.example.starwarsquiz.feature.question
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.starwarsquiz.core.model.data.Person
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class QuestionViewModel @Inject constructor(
     private val questionInteractor: QuestionInteractor
-): ViewModel()
+) : ViewModel() {
+
+    val questionUiState: StateFlow<QuestionUiState> = questionUiState()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000), // TODO
+            initialValue = QuestionUiState.Loading
+        )
+}
+
+private fun questionUiState(): Flow<QuestionUiState> {
+    TODO()
+}
+
+sealed interface QuestionUiState {
+    data class Success(val personList: List<Person>) : QuestionUiState
+    object Error : QuestionUiState
+    object Loading : QuestionUiState
+}
